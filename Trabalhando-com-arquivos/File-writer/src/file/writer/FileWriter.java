fbc905b4c236a51dcebd92b17f181ace169732a8
Bruno Piovan <bpiovan90@gmail.com>
2024-09-19 18:31:23 -0300

    Buffered writer

--- Trabalhando-com-arquivos/File-writer/src/file/writer/FileWriter.java
+++ Trabalhando-com-arquivos/File-writer/src/file/writer/FileWriter.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package file.writer;
 
-/**
- *
- * @author bpiov
- */
+import java.io.BufferedWriter;
+import java.io.IOException;
+
 public class FileWriter {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String [] lines = new String[]{"Good morning","Good afternoon","Good night"};
+        
+        String path = "c:\\temp\\out.txt";
+        
+        try(BufferedWriter bw = new BufferedWriter(new java.io.FileWriter(path))){
+            for(String line : lines){
+                bw.write(line);
+                bw.newLine();
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
     
 }
